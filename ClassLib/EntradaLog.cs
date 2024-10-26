644057f46ec722f5a266468cb752b4c915132c01
IML1012 <118357948+iml1012@users.noreply.github.com>
2024-10-26 13:24:15 +0200

    Se publica la clase EntradaLog
    Se publica la clase EntradaLog para la gestión de Logs.

---
+++ ClassLib/EntradaLog.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLib
+{
+    internal class EntradaLog
+    {
+        DateTime _fechaEvento;
+        string _usuario;
+        string _evento;
+    
+        public EntradaLog (string usuario, string evento)
+        {
+            _fechaEvento = DateTime.Now;
+            Usuario = usuario;
+            Evento = evento;
+        }
+
+        public DateTime FechaEvento { get => _fechaEvento;}
+        public string Usuario { get => _usuario; set => _usuario = value; }
+        public string Evento { get => _evento; set => _evento = value; }
+    }
+}
