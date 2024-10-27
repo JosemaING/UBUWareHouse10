b4cb4f8f7b701ee1056ed0197c0d8012019a61cf
Jose M. Santos <57809308+JosemaING@users.noreply.github.com>
2024-10-27 21:03:41 +0100

    Primera implementación del proyecto de Tests del formulario
    Se crea una primera estructura y visión del proyecto para los tests de los formularios, se realizará con Selenium

---
+++ wwwTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("wwwTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP")]
+[assembly: AssemblyProduct("wwwTest")]
+[assembly: AssemblyCopyright("Copyright © HP 2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4aa6f164-ddde-4972-bc03-d882b56ec5b7")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
