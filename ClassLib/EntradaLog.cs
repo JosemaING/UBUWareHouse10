33fb1a34872b857425e67f5a4bf65801756a3bdf
IML1012 <118357948+iml1012@users.noreply.github.com>
2024-10-28 19:02:35 +0100

    Migración de NET 8.0 a NET Framework 4.7.2
    Migración del proyecto ClassLib

---
+++ ClassLib/EntradaLog.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLib
+{
+    internal class EntradaLog
+    {
+        private DateTime _fechaEvento;
+        private string _usuario;
+        private string _evento;
+    
+        public EntradaLog (string usuario, string evento)
+        {
+            _fechaEvento = DateTime.Now;
+            Usuario = usuario;
+            Evento = evento;
+        }
+
+        public DateTime FechaEvento { get { return _fechaEvento;}}
+        public string Usuario { get => _usuario; set => _usuario = value; }
+        public string Evento { get => _evento; set => _evento = value; }
+        
+    }
+}
---
+++ ClassLibTests/UsuarioTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ClassLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLib.Tests
+{
+    [TestClass()]
+    public class UsuarioTests
+    {
+        [TestMethod()]
+        public void UsuarioTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
